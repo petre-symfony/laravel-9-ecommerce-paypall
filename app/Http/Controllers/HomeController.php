87ebdf7c1bc444e887fe6f08935e5a61de39f70d
petrero <petrero@gmail.com>
2022-08-31 22:47:42 +0300

    1.15 Introduction - Create menu page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller {
+    /**
+     * Create a new controller instance
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth');
+    }
+
     public function shop() {
         return view('front.shop');
     }
--- resources/views/front/master.blade.php
+++ resources/views/front/master.blade.php
@@ -77,44 +77,8 @@
         </div>
     </div>
 </header>
-<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <a href={{ url('/') }} class="navbar-brand" href="#">
-        <img src="{{ URL::asset('dist/img/ecom.png') }}" alt=""/>
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="{{ url('/') }}">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/shop') }}">Shop</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-        </ul>
-        <li class="list-inline-item">
-            <a href="{{ url('/login') }}">Login</a>
-        </li>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    </div>
-</nav>
-
+@include('front.menu')
 @yield('content')
 
 <footer class="text-muted">
