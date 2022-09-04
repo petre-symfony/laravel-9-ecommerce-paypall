384f67a34f66d5170af8435d31bc4693a1361fac
petrero <petrero@gmail.com>
2022-09-04 10:35:56 +0300

    1.23 Introduction - Design Product details Page

--- resources/views/front/product_details.blade.php
+++ resources/views/front/product_details.blade.php
@@ -1,7 +1,19 @@
 @extends('front.master')
 
 @section('content')
-    <div>{{ $products }}</div>
+    details page
+
+    <div style="width: 20rem;">
+        <img src="{{ URL::asset('images/' . $products->image) }}" alt="Card Image Cap">
+        <div>
+            <h4>Card Title</h4>
+            <p></p>
+            <a href="Read More"></a>
+        </div>
+    </div>
+    <div class="product-information">
+        <h2><?php echo ucwords($products->pro_name) ?></h2>
+    </div>
 @endsection
 
 
