c58ab033262c2b01fa94c49b6030d3d26d6b5c9d
petrero <petrero@gmail.com>
2022-09-19 00:11:16 +0300

    1.49 Introduction - Design category page

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,52 @@
+@extends('admin.master')
+@section('content')
+    <div class="navbar">
+        <a href="" class="navbar-brand">Categories</a>
+
+        <ul class="nav navbar-nav">
+            @if(!empty($categories))
+                @forelse($categories as $category)
+                    <li class="active">
+                        <a href="{{ route('category.show', [
+	                      'id' => $category->id
+                        ]) }}">{{ $category->name }}</a>
+                    </li>
+                @empty
+                    <li>No Items</li>
+                @endforelse
+            @endif
+        </ul>
+    </div>
+    {!! Form::open(['route' => 'category.store', 'method' => 'post']) !!}
+        <div class="form-group">
+            {{ Form::label('name', 'Title') }}
+            {{ Form::text('name', null, array('class' => 'form-control')) }}
+        </div>
+
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
+    {!! Form::close() !!}
+
+    {{--products--}}
+    @if(isset($products))
+        <h3>Products</h3>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th>Products</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($products as $product)
+                <tr>
+                    <td>{{ $product->name }}</td>
+                </tr>
+                @empty
+                <tr>
+                    <td>No data</td>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
+    @endif
+@endsection
