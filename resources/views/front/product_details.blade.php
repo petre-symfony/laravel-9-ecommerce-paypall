9d02801b54288470c42d35419332a029cd2e345b
petrero <petrero@gmail.com>
2022-09-19 01:02:07 +0300

    1.52 Introduction - Design Products Detail Page

--- resources/views/front/product_details.blade.php
+++ resources/views/front/product_details.blade.php
@@ -1,18 +1,64 @@
 @extends('front.master')
 
 @section('content')
-    details page
+    <header>
+        <div class="container align-vertical hero">
+            <div class="row">
+                <div class="col-md-5 text-left">
 
-    <div style="width: 20rem;">
-        <img src="{{ URL::asset('images/' . $products->image) }}" alt="Card Image Cap">
-        <div>
-            <h4>Card Title</h4>
-            <p></p>
-            <a href="Read More"></a>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="product-information">
-        <h2><?php echo ucwords($products->pro_name) ?></h2>
+    </header>
+
+    <section id="index-amazon">
+        <div class="amazon">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="product">
+                            <div class="row">
+                                <div class="col-md-6 col-xs-12">
+                                    <div class="thumbnail">
+                                        <img src="{{ URL::asset('images/' . $products->image) }}" alt="" class="card-img">
+                                    </div>
+                                </div>
+                                <div class="col-md-5 col-md-offset-1">
+                                    <div class="product-details">
+                                        <h2 class="product-title">
+                                            <h2><?php echo ucwords($products->pro_name)?></h2>
+                                        </h2>
+
+
+                                        <a href="{{
+                                          route('add_item_to_cart', [
+                                            'id' => $products->id
+                                          ]) }}"
+                                           class="add-to-cart btn btn-primary btn-sm"
+                                        >
+                                            Add to Cart<i class="fa fa-shopping-cart"></i>
+                                        </a>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="no-padding-top section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="" class="load-more">
+                        <i class="fa fa-ellipsis-h"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
